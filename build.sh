afd10b5c8fae088fe84e6a2a275897e63d3a05ac
shanezhu100-svg <shanezhu100@gmail.com>
2026-08-15 11:21:17 -0700

    Run UI action bridge finalizer and gates

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ python3 p2_finalize.py
 python3 test_p2_output.py
 python3 security_finalize.py
 python3 test_security_hotfix_output.py
+python3 ui_action_finalize.py
+python3 test_ui_action_output.py
