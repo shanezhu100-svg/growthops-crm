3beb8fe6b41580a8890b63a21b3de4795fcffa61
shanezhu100-svg <shanezhu100@gmail.com>
2026-08-16 03:18:09 -0700

    Run admin credential unlock v4 gate

--- build.sh
+++ build.sh
@@ -22,6 +22,8 @@ python3 test_security_reveal_ui_output.py
 python3 test_credential_template_initial_output.py
 python3 test_account_login_identifier_output.py
 python3 test_credential_field_reveal_v3_output.py
+python3 credential_unlock_v4_finalize.py
+python3 test_credential_unlock_v4_output.py
 python3 cloud_save_queue_finalize.py
 python3 test_cloud_save_queue_output.py
 python3 ui_action_finalize.py
