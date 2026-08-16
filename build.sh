0d2e4bb2db42d453059d93adb2a4b586b5714a8c
shanezhu100-svg <shanezhu100@gmail.com>
2026-08-16 02:55:42 -0700

    Use corrected safe login identifier finalizer

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python3 credential_status_ui_finalize.py
 python3 account_asset_client_context_finalize.py
 python3 credential_status_loading_finalize.py
 python3 account_asset_value_typography_finalize.py
-python3 account_login_identifier_finalize.py
+python3 account_login_identifier_v2_finalize.py
 python3 test_security_hotfix_output.py
 python3 test_security_reveal_ui_output.py
 python3 test_credential_template_initial_output.py
