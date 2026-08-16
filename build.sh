ae984f38323d88c8f0299199f7d55c7a8336d7c7
shanezhu100-svg <shanezhu100@gmail.com>
2026-08-15 19:49:16 -0700

    Run serialized cloud save queue gates

--- build.sh
+++ build.sh
@@ -6,5 +6,7 @@ python3 p2_finalize.py
 python3 test_p2_output.py
 python3 security_finalize.py
 python3 test_security_hotfix_output.py
+python3 cloud_save_queue_finalize.py
+python3 test_cloud_save_queue_output.py
 python3 ui_action_finalize.py
 python3 test_ui_action_output.py
