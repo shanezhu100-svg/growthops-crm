d7363280ceb873e9b4bec068664745dca151a161
shanezhu100-svg <shanezhu100@gmail.com>
2026-08-15 21:27:46 -0700

    Run direct client navigation gates

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ python3 cloud_save_queue_finalize.py
 python3 test_cloud_save_queue_output.py
 python3 ui_action_finalize.py
 python3 test_ui_action_output.py
+python3 ui_client_navigation_finalize.py
+python3 test_ui_client_navigation_output.py
