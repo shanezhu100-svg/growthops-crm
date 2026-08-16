5bf8cbaeafdfc0b952bdbcdce5d7ce1eafb90b3c
shanezhu100-svg <shanezhu100@gmail.com>
2026-08-16 03:46:27 -0700

    Gate session and unlock security rules

--- build.sh
+++ build.sh
@@ -31,6 +31,7 @@ python3 test_workspace_state_secret_hard_guard.py
 python3 test_backup_security_output.py
 python3 test_vercel_security_headers.py
 python3 test_rpc_surface_hardening.py
+python3 test_session_security_migrations.py
 python3 cloud_save_queue_finalize.py
 python3 test_cloud_save_queue_output.py
 python3 ui_action_finalize.py
