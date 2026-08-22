316cfef59fd94a060f7a8893c4745f6d7992a2de
shanezhu100-svg <shanezhu100@gmail.com>
2026-08-23 01:16:35 +0800

    Refresh P1 hash for ephemeral reveal rehydration

--- cloudflare_p1_verify.py
+++ cloudflare_p1_verify.py
@@ -9,7 +9,7 @@ DIST = ROOT / 'dist'
 EXPECTED_SHA256 = {
     'index.html': '941be51fcaf60acd0bb350c1822260f24555340fb2d719effe0f339c3b69a1e5',
     'cloud-adapter.js': '2a5b5da0f94ba66a2b58ed64b923e0167e7723eb7ccccd3c6384dfbeb471a2a6',
-    'cloud-security-hotfix.js': 'a562a837ce8417eddb1b5c184143a2c8b9c057afd3a66adc2113ab1cb9dfa3c9',
+    'cloud-security-hotfix.js': 'f2b3f08c9bbabc4e974c859fe6d86396d028f46b43354b6d74572b5efa938194',
     'cloud-p1-overrides.js': 'e50e05322a0d56e78bf112a52be08ff54263f4ce88cb0b9b91f6613722b8ccab',
     'cloud-ui-action-bridge.js': 'b15e0b792e2f0ba6e99bef53fea96dde78b647b5528ae199311c4be9b37027a7',
 }
