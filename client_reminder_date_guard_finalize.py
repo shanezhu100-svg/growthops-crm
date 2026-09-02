ddfaf052d13bdb311c779232ed1e8c3aa73c9b24
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-02 21:38:46 +0800

    Validate renewal calendar dates

--- client_reminder_date_guard_finalize.py
+++ client_reminder_date_guard_finalize.py
@@ -72,7 +72,7 @@ recharge_row_old = "date:this.rechargeForm.date||this.localDateKey()"
 recharge_row_new = "date:rechargeDate"
 
 renewal_assign = "const newDue=String(this.renewalForm.newDueDate||'');"
-renewal_empty = "if(!newDue){this.notify('请选择新的到期日');return}"
+renewal_empty = "if(!newDue){this.notify('请选择新的到期日期');return}"
 renewal_empty_guarded = renewal_empty + calendar_guard('newDue', '请选择有效的到期日期', 'renewalDate')
 
 found = {'saveRecharge': 0, 'saveRenewal': 0}
@@ -105,8 +105,6 @@ for path in files:
         if source.count(renewal_assign) != 1:
             fail(f'saveRenewal assignment anchor count={source.count(renewal_assign)}')
         if source.count(renewal_empty) != 1:
-            probe_at = source.find(renewal_assign)
-            print('CLIENT_REMINDER_RENEWAL_PREFIX_PROBE=' + repr(source[probe_at:probe_at + 700]))
             fail(f'saveRenewal empty-date anchor count={source.count(renewal_empty)}')
         patched = source.replace(renewal_empty, renewal_empty_guarded, 1)
         text = text[:start] + patched + text[end:]
