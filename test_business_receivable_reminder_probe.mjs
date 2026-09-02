8604b39689e2e8c704d830d06c8b4177ac0921d7
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-02 21:48:31 +0800

    Wire bulk receivable probe

--- test_business_receivable_reminder_probe.mjs
+++ test_business_receivable_reminder_probe.mjs
@@ -10,3 +10,4 @@ await import('./test_business_opening_cost_month_lock.mjs');
 await import('./test_business_opening_deal_mutations.mjs');
 await import('./test_business_client_reminder_money_mutations.mjs');
 await import('./test_business_client_reminder_date_mutations.mjs');
+await import('./test_business_bulk_receivable_probe.mjs');
