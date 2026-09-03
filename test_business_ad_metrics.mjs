6af5d8ebae9bcd4e395ab79034d91ae0d7e1992e
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-03 20:51:40 +0800

    Reach permanent logout persistence barrier regression

--- test_business_ad_metrics.mjs
+++ test_business_ad_metrics.mjs
@@ -87,4 +87,4 @@ await import('./test_business_financial_input_bounds.mjs');
 await import('./test_business_ad_input_bounds.mjs');
 await import('./test_business_adapter_confirmation_integrity.mjs');
 await import('./test_business_backup_persistence_ack.mjs');
-await import('./test_business_logout_persistence_barrier_probe.mjs');
+await import('./test_business_logout_persistence_barrier.mjs');
