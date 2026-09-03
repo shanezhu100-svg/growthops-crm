c145d905a6837b3c300808078fd2a239645742ee
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-03 21:04:55 +0800

    Wire logout audit truthfulness finalizer

--- finance_cost_input_guard_finalize.py
+++ finance_cost_input_guard_finalize.py
@@ -9,6 +9,7 @@ import dismissed_alert_restore_integrity_finalize  # noqa: F401
 import adapter_confirmation_integrity_finalize  # noqa: F401
 import backup_persistence_ack_finalize  # noqa: F401
 import logout_persistence_barrier_finalize  # noqa: F401
+import logout_audit_truthfulness_finalize  # noqa: F401
 
 ROOT = Path(__file__).resolve().parent
 APP_DIR = ROOT / 'dist' / 'app'
