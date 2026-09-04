39e1806be711ec02ff9c00a0300b84f77d64457c
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-04 10:41:12 +0800

    Wire permanent receivable persistence ACK regression

--- test_business_receivable_payment_bounds_probe.mjs
+++ test_business_receivable_payment_bounds_probe.mjs
@@ -4,4 +4,4 @@ import './test_business_receivable_payment_bounds.mjs';
 import './test_business_receivable_payment_date_probe.mjs';
 import './test_business_destructive_confirmation_integrity.mjs';
 import './test_business_receivable_payment_persistence_ack.mjs';
-import './test_business_receivable_persistence_ack_probe.mjs';
+import './test_business_receivable_persistence_ack.mjs';
