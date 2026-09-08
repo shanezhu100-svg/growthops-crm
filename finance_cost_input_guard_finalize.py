b8c9cc645f0019a8960c03dd2b8d3458bae7ec02
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-08 14:22:42 +0800

    Wire ad plan persistence ACK finalizer

--- finance_cost_input_guard_finalize.py
+++ finance_cost_input_guard_finalize.py
@@ -17,6 +17,7 @@ import lead_save_persistence_ack_finalize  # noqa: F401
 import lead_link_repair_persistence_ack_finalize  # noqa: F401
 import resource_catalog_persistence_ack_finalize  # noqa: F401
 import ad_structure_persistence_ack_finalize  # noqa: F401
+import ad_plan_persistence_ack_finalize  # noqa: F401
 
 ROOT = Path(__file__).resolve().parent
 APP_DIR = ROOT / 'dist' / 'app'
