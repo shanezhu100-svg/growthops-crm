8ad1a103d2184e4761f99eda9bb89d9b93e5c477
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-08 11:26:36 +0800

    Wire temporary ad structure diagnostic

--- finance_cost_input_guard_finalize.py
+++ finance_cost_input_guard_finalize.py
@@ -4,6 +4,7 @@ import re
 
 import resource_catalog_integrity_finalize  # noqa: F401
 import ad_structure_integrity_finalize  # noqa: F401
+import ad_structure_method_diagnostic_finalize  # noqa: F401
 import sop_mutation_integrity_finalize  # noqa: F401
 import dismissed_alert_restore_integrity_finalize  # noqa: F401
 import adapter_confirmation_integrity_finalize  # noqa: F401
