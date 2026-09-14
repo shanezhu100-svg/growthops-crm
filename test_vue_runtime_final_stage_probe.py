a8dec4783f7209313b1d52c65498d0a2f88f9da1
shanezhu100-svg <shanezhu100@gmail.com>
2026-09-14 22:44:40 +0800

    Sync reviewed final-stage contract reminder template pin

--- test_vue_runtime_final_stage_probe.py
+++ test_vue_runtime_final_stage_probe.py
@@ -7,7 +7,7 @@ ROOT = Path(__file__).resolve().parent
 INDEX = ROOT / 'dist' / 'index.html'
 APP_FILES = [ROOT / 'dist' / 'app' / f'app-inline-{idx:02d}.js' for idx in range(1, 4)]
 EXPECTED_TEMPLATES = {
-    'root': ('cdfc7d20145c9e9a633ecf4d9515346cdcc8c363a1cb28aa44b3f7a4f653b047', 356707),
+    'root': ('f163c479e278e7e26083d8f89082a1ba33ca58e0d2fab70bbf0771dce7132a9f', 357018),
     'component01': ('2f51f5b5ec5ef5bbe12bac62b317a4ad4154cb545779ef8cecb908d016642088', 461),
     'component02': ('f53ef37adfd6f610d2419ab6872195fed96961e80706d572341c923643f7e3f8', 196),
     'component03': ('abceefaa3412391b9b1d384e543144f7b8e2fa30384b9cfd38b1cbb09aeaa788', 126),
